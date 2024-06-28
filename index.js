54f18031f6bbbfd8b02b315215615610f282bf1f
David Wekesa <wekesadavid300@gmail.com>
2024-06-28 20:50:35 +0300

    Strings

--- index.js
+++ index.js
@@ -1,1 +1,4 @@
-// Write your code in this file!
+const currentUser = "David Wekesa";
+const welcomeMessage = "Welcome to Flatbook,  David Wekesa!";
+const excitedWelcomeMessage = welcomeMessage.toUpperCase();
+const shortGreeting = `Welcome, ${currentUser.slice(0, 1)}!`;
